b46c140fbe05d784752dadad59e5f2b48b0ae55e
morty <jsmorty@itcom888.com>
2021-01-04 22:16:11 +0800

    gradient_decent

---
+++ gradient_decent.py
@@ -0,0 +1,50 @@
+import random
+import matplotlib.pyplot as plt
+
+true_theta0 = random.random()
+true_theta1 = random.random()
+points = []
+x = []
+y = []
+n = 100
+def generate_dots(n):
+    global points
+    for i in range(1, n):
+        p = random.randint(0,100)
+        x.append(p)
+        y.append(true_theta0 + true_theta1 * p + (random.random() - 0.5) * 5)
+    points = list(zip(x, y))
+
+generate_dots(n)
+
+theta0 = 1
+theta1 = 1
+alpha = 0.000001
+
+def cost_func_derivative0():
+    p = 0
+    for point in points:
+        p += (theta0 + theta1 * point[0] - point[1]) * 2
+    return p / len(points)
+
+def cost_func_derivative1():
+    p = 0
+    for point in points:
+        p += (theta0 + theta1 * point[0] - point[1]) * 2 * point[0]
+    return p / len(points)
+
+def calc_theta():
+    global theta0, theta1
+    for i in range(1, 1000):
+        print(cost_func_derivative0())
+        temp0 = theta0 - alpha * cost_func_derivative0()
+        temp1 = theta1 - alpha * cost_func_derivative1()
+        theta0 = temp0
+        theta1 = temp1
+    print(theta0, theta1, true_theta0, true_theta1)
+    t = [theta0 + theta1 * i for i in x]
+    plt.plot(x, y, 'ro')
+    plt.plot(x, t)
+    plt.show()
+
+calc_theta()
